53b0a22d741e59dc1395c2bc29a91a1c4dd42511
RonenSiv <ronensivak@gmail.com>
2023-06-08 22:17:16 +0300

    added public to server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const app = express();
 const port = 3001;
 const Chat = require("./models/chat");
 
-
-
 const cors = require("cors");
 app.use(cors());
 
@@ -17,10 +15,9 @@ app.use(cors(corsOptions));
 const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
-app.use(express.static('public'));
+app.use(express.static("public"));
 // app.use('/SignUp', express.static('public'));
-app.use('/chat', express.static('public'));
-
+app.use("/chat", express.static("public"));
 
 const io = new Server(server, {
   cors: {
